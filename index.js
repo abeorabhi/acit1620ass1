2b74a3e8ea075ecce7432cd814e70abbbe5b3c48
abeorabhi <abhiroopsinha1@gmail.com>
2018-02-05 15:15:49 -0800

    Made some final changes to index.js

--- index.js
+++ index.js
@@ -3,24 +3,28 @@ var zoomh = 70;
 counter = 1;
 
 document.getElementById("bg1").addEventListener("click", function(){
+    counter = 1;
     document.getElementById("ch1").style.backgroundImage = "url(imgs/i1.jpg)";
     document.getElementById("ch2").style.backgroundImage = "url(imgs/i2.jpg)";
     document.getElementById("ch3").style.backgroundImage = "url(imgs/i3.jpg)";
 })
 
 document.getElementById("bg2").addEventListener("click", function(){
+    counter = 2;
     document.getElementById("ch1").style.backgroundImage = "url(imgs/i4.jpg)";
     document.getElementById("ch2").style.backgroundImage = "url(imgs/i5.jpg)";
     document.getElementById("ch3").style.backgroundImage = "url(imgs/i6.jpg)";
 })
 
 document.getElementById("bg3").addEventListener("click", function(){
+    counter = 3;
     document.getElementById("ch1").style.backgroundImage = "url(imgs/i7.jpg)";
     document.getElementById("ch2").style.backgroundImage = "url(imgs/i8.jpg)";
     document.getElementById("ch3").style.backgroundImage = "url(imgs/i9.jpg)";
 })
 
 document.getElementById("bg4").addEventListener("click", function(){
+    counter = 4;
     document.getElementById("ch1").style.backgroundImage = "url(imgs/i10.jpg)";
     document.getElementById("ch2").style.backgroundImage = "url(imgs/i11.jpg)";
     document.getElementById("ch3").style.backgroundImage = "url(imgs/i12.jpg)";
